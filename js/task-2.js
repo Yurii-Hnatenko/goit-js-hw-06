6d4674b82111b0ac2eac5c9c53b1e9ac01596e4d
Yurii-Hnatenko <gnatenkourij573@gmail.com>
2024-02-21 10:03:41 +0200

    hw-6-v.1.2

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,20 @@
 class Storage {
   constructor(items) {
-    this._items = items;
+    this.items = items;
   }
 
   getItems() {
-    return this._items;
+    return this.items;
   }
 
   addItem(newItem) {
-    this._items.push(newItem);
+    this.items.push(newItem);
   }
 
   removeItem(itemToRemove) {
-    const index = this._items.indexOf(itemToRemove);
+    const index = this.items.indexOf(itemToRemove);
     if (index !== -1) {
-      this._items.splice(index, 1);
+      this.items.splice(index, 1);
     }
   }
 }
